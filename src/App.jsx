5640d84ff72696fc9471b4541ba1292e23d99c4e
yashdotdev <thisisyash459@gmail.com>
2025-11-04 19:04:52 +0530

    Add Product detail route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
-import ProductCard from "./components/ProductCard/ProductCard";
-import { Header } from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import { ProductDetail } from "./pages/ProductDetail/ProductDetail";
 import { Cart } from "./pages/Cart/Cart";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/cart" element={<Cart />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="product/:id" element={<ProductDetail />} />
     </Routes>
   );
 }
